ea4951c888846a09b6bf88e06172c5874b5c91e3
Mark Carrington <frumarkcarrington@gmail.com>
2024-05-27 06:14:39 +0530

    built the user login function updated the api link

--- server/routes/controllers.js
+++ server/routes/controllers.js
@@ -12,4 +12,15 @@ const userRegistration = (req, res) => {
   res.send("hello user");
 };
 
-module.exports = { userRegistration };
+const userLogin = (req, res) => {
+  const { email, password } = req.body;
+  if (email || password) {
+    return res.json({
+      success: true,
+      data: { email: email, password: password },
+    });
+  }
+  res.send("hello user");
+};
+
+module.exports = { userRegistration, userLogin };
--- server/routes/routers.js
+++ server/routes/routers.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const { userRegistration } = require("./controllers");
+const { userRegistration, userLogin } = require("./controllers");
 const routes = express.Router();
 
 routes.post("/registration", userRegistration);
+routes.post("/login", userLogin);
 
 module.exports = routes;
