5d605f13da24ed22b9d3682dca742ad09025ea52
Mark Carrington <frumarkcarrington@gmail.com>
2024-05-27 19:02:56 +0530

    wrote the code to create the user in the express user registration server function added the route to handle the confirmation that the user has been created

--- server/routes/controllers.js
+++ server/routes/controllers.js
@@ -1,15 +1,36 @@
 // this file is for all the server controller functions
+const UserModel = require("../data/userSchema");
 
-const userRegistration = (req, res) => {
-  const { username, email, password } = req.body;
-  console.log("user data:", username, email, password);
-  if (username || email || password) {
-    return res.json({
-      success: true,
-      data: { username: username, email: email, password: password },
-    });
+const userRegistered = (req, res) => {
+  return res
+    .status(200)
+    .json({ success: true, message: "User was successfully created" });
+};
+
+const userRegistration = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await createUserModel(username, email, password);
+    const savedUser = await user.save();
+    if (savedUser) return res.redirect("/users/registration/success");
+    else return next(Error("Failed to save user"));
+  } catch (error) {
+    return next(error);
   }
-  res.send("hello user");
+};
+
+const createUserModel = async (name, email, password) => {
+  try {
+    const user = new UserModel({
+      username: name,
+      email: email,
+      password: password,
+    });
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const userLogin = (req, res) => {
@@ -23,4 +44,4 @@ const userLogin = (req, res) => {
   res.send("hello user");
 };
 
-module.exports = { userRegistration, userLogin };
+module.exports = { userRegistered, userRegistration, userLogin };
