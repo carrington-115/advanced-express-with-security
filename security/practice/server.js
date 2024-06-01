4a3961bacc1ef820324aeb5551ad0331697c9176
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-01 21:17:53 +0530

    setup the server for receiving responses and sending requests

--- security/practice/server.js
+++ security/practice/server.js
@@ -2,3 +2,10 @@ require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.MONGODB_API);
+const app = express();
+app.use([express.json(), express.urlencoded({ extended: true })]);
+
+app.listen(process.env.PORT, (error) => {
+  if (error) console.error(error);
+  console.log("The app is running on port", process.env.PORT);
+});
