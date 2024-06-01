2cc1150cdaf7ba7fb1c71b8afdd3d7b443b838db
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-01 21:54:43 +0530

    built the code to set and get cookies through the node-express server. * processed the code with cookieParser with the req and res * the res is used to sent the cookie using the res.cookie() * the req is used to receive the cookie using the req.cookies.cookieName

--- security/practice/server.js
+++ security/practice/server.js
@@ -1,9 +1,31 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "../../.env" });
 const express = require("express");
-const { MongoClient } = require("mongodb");
-const client = new MongoClient(process.env.MONGODB_API);
+// const { MongoClient } = require("mongodb");
+// const client = new MongoClient(process.env.MONGODB_API);
 const app = express();
-app.use([express.json(), express.urlencoded({ extended: true })]);
+const cookieParser = require("cookie-parser");
+
+// pass all the middlewares
+app.use([
+  express.json(),
+  express.urlencoded({ extended: true }),
+  cookieParser(),
+]);
+
+app.get("/set-cookie", (req, res) => {
+  res.cookie("username", "Mark Carrington", { maxAge: 900000, httpOnly: true });
+  res.send("Cookie has been sent");
+});
+
+app.get("/get-cookie", (req, res) => {
+  const username = req.cookies.username;
+  if (username) {
+    return res
+      .status(200)
+      .json({ success: true, data: { username: username } });
+  }
+  res.send("The cookie data has been received");
+});
 
 app.listen(process.env.PORT, (error) => {
   if (error) console.error(error);
