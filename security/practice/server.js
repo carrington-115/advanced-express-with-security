3b05687a9e8d3ae8ea3709f23b24af73fba8b9de
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-02 04:19:58 +0530

    Installed the body-parser package refresh the initial server code

--- security/practice/server.js
+++ security/practice/server.js
@@ -25,40 +25,6 @@ app.use([
   }),
 ]);
 
-const setTheConnectServerFunction = async () => {
-  try {
-    const clientConnect = await client.connect();
-    console.log("The client has successfully connected to the server");
-    const sessionDb = client.db(dbName);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-setTheConnectServerFunction();
-
-app.get("/set-session", (req, res) => {
-  req.session.username = "Mark Carrington";
-  res.send("The session has been created");
-});
-
-app.get("/get-session", (req, res) => {
-  const username = req.session.username;
-  if (username) {
-    return res.status(200).json({ success: true, username: username });
-  }
-  res.send("No session data");
-});
-
-app.get("/destroy-session", (req, res) => {
-  req.session.destroy((error) => {
-    if (error) {
-      return res.send("An error occured");
-    }
-    res.send("The session data is destroyed");
-  });
-});
-
 app.listen(process.env.PORT, (error) => {
   if (error) console.error(error);
   console.log("The app is running on port", process.env.PORT);
